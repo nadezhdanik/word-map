f5cc54de1e448d7e16ba7f7e9a4063536ee148a5
Alena Alekseeva <a77993519@gmail.com>
2025-09-11 14:01:48 +0400

    refactor(core, registration): add registration

--- src/app/core/services/auth.ts
+++ src/app/core/services/auth.ts
@@ -1,8 +1,31 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { Auth as FirebaseAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, UserCredential, onAuthStateChanged } from '@angular/fire/auth';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class Auth {
-  
+  public isUserLoggedIn = signal<boolean>(false);
+
+  private auth = inject(FirebaseAuth);
+
+  constructor() {
+    onAuthStateChanged(this.auth, (user) => {
+      this.isUserLoggedIn.set(!!user);
+    });
+  }
+
+  public async register(email: string, password: string): Promise<UserCredential> {
+    return await createUserWithEmailAndPassword(this.auth, email, password);
+  }
+
+  public async login(email: string, password: string): Promise<UserCredential> {
+    return await signInWithEmailAndPassword(this.auth, email, password);
+  }
+
+  public async logout(): Promise<void> {
+    return await signOut(this.auth);
+  }
 }
+
--- src/app/features/registration/registration.ts
+++ src/app/features/registration/registration.ts
@@ -1,12 +1,76 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RegistrationForm } from './models/registration-form.model';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from '@angular/material/button';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { Auth } from '../../core/services/auth';
 
 @Component({
   selector: 'app-registration',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatProgressSpinnerModule
+  ],
   templateUrl: './registration.html',
   styleUrl: './registration.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class Registration {
+export class Registration implements OnInit {
+  public registrationForm!: FormGroup<RegistrationForm>
+  public isPasswordHidden = true;
+  public errorMessage: string | null = null;
+  public isLoading = false;
 
+
+  private router = inject(Router);
+  private fb = inject(FormBuilder);
+  private authService = inject(Auth);
+
+
+  public ngOnInit(): void {
+    this.createRegistrationForm()
+  }
+
+  public async onSubmit(): Promise<void>{
+    if (this.registrationForm.invalid) return;
+
+    this.isLoading = true;
+    this.errorMessage = null;
+
+    const { email, password } = this.registrationForm.getRawValue();
+
+    try {
+      await this.authService.register(email, password);
+      await this.router.navigate(['/home']);
+    } catch {
+      this.errorMessage = 'Error Registration';
+    } finally {
+      this.isLoading = false;
+    }
+  }
+
+  public togglePasswordVisibility(): void {
+    this.isPasswordHidden = !this.isPasswordHidden
+  }
+
+  private createRegistrationForm(): void {
+    this.registrationForm = this.fb.nonNullable.group<RegistrationForm>({
+      email: this.fb.nonNullable.control('', [
+        Validators.required,
+        Validators.email,
+      ]),
+      password: this.fb.nonNullable.control('', [
+        Validators.required,
+        Validators.minLength(6),
+      ])
+    })
+  }
 }
