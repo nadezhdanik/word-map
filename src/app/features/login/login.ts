f506f503189f10af5411d969b42492f320a9ae49
nadezhdanik <nadezhdanik@gmail.com>
2025-09-14 12:27:49 +0300

    feat: add validation error messages, implement hiding password

--- src/app/features/login/login.ts
+++ src/app/features/login/login.ts
@@ -5,16 +5,19 @@ import { EMAIL_PATTERN } from '../../core/patterns/email-pattern';
 import { PASSWORD_PATTERN } from '../../core/patterns/password-pattern';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInput],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInput, MatButtonModule, MatIcon],
   templateUrl: './login.html',
   styleUrl: './login.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Login implements OnInit {
   public loginForm!: FormGroup<LoginForm>;
+  public isPasswordHidden = true;
 
   private fb = inject(FormBuilder);
 
@@ -24,6 +27,10 @@ export class Login implements OnInit {
 
   public onSubmit(): void {
     return;
+  }
+
+  public togglePasswordVisibility(): void {
+    this.isPasswordHidden = !this.isPasswordHidden;
   }
 
   private createLoginForm(): void {
