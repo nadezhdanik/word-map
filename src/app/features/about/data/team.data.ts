abb75ff0fa7f113ce2e51047a01cc4750f778456
TatsHats <6642017@gmail.com>
2025-09-14 20:39:43 +0300

    feat(about): add about component with interactive cards

---
+++ src/app/features/about/data/team.data.ts
@@ -0,0 +1,29 @@
+export const TEAM = [
+    {
+      name: 'Tatsiana Hatskaya',
+      role: 'Team lead, frontend-developer',
+      avatar: '3.png',
+      about: 'I had to rethink my entire career. Now I write code - it doesn’t always work, but at least it’s safe.',
+      stack: 'Angular, Figma, Canva, Firebase',
+      gitHub: 'https://github.com/TatsHats',
+      contribution: ['Home Page', 'About Page', 'Words data base', 'Game 1'],
+    },
+    {
+      name: 'Nadezhda Memelova',
+      role: 'Frontend-developer',
+      avatar: '2.png',
+      about: 'I studied Russian, Belarusian, English, German, Italian, Lithuanian... But the language that won my heart is JavaScript.',
+      stack: 'Angular, Firebase, ....',
+      gitHub: 'https://github.com/nadezhdanik',
+      contribution: ['Login Page', 'Repository setup', 'Deploy', 'Environment Config', 'Game 2'],
+    },
+    {
+      name: 'Alekseeva Alena',
+      role: 'Frontend-developer',
+      avatar: '1.png',
+      about: 'Degreed economics educator. Years in business.Yet my fingers kept tracing lines of code — programming was the love that wouldn’t let go.',
+      stack: 'Angular, Figma, Firebase, ....',
+      gitHub: 'https://github.com/Alena1409',
+      contribution: ['Home Page', 'Header', 'Footer', 'GitHub Board', 'Game 3'],
+    },
+];
