d997858ad11455eb54ce2cce4622f1500e14dbf3
Alena Alekseeva <a77993519@gmail.com>
2025-09-17 16:33:07 +0400

    refactor(match-pairs): add game match-pairs

--- src/app/features/category/features/match-pairs/match-pairs.ts
+++ src/app/features/category/features/match-pairs/match-pairs.ts
@@ -1,12 +1,17 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { Card } from './models/card.interface';
 import { TYPE } from './models/card-type.enum';
 import { FormsModule } from '@angular/forms';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIcon } from '@angular/material/icon';
+import { CategoryServiceMock } from '../../../home/services/categories.service.mock';
+import { ActivatedRoute } from '@angular/router';
+import { Word } from '../../../home/interfaces/word.interface';
+
 
 @Component({
   selector: 'app-match-pairs',
-  imports: [ FormsModule, MatButtonToggleModule],
+  imports: [FormsModule, MatButtonToggleModule, MatIcon],
   templateUrl: './match-pairs.html',
   styleUrl: './match-pairs.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -14,26 +19,28 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle';
 export class MatchPairs implements OnInit {
   public cards: Card[] = [];
   public easyMode = false;
+  public gameWon = false;
+  public words: Word[] = [];
 
-  public WORDS = [
-    { category: "Question words", level: "A1", partOfSpeech: "phrase", translation: "какого рода", word: "what kind" },
-    { category: "Common verbs", level: "A1", partOfSpeech: "verb", translation: "делать", word: "to do" },
-    { category: "Common verbs", level: "A1", partOfSpeech: "verb", translation: "идти", word: "to go" },
-    { category: "Colors", level: "A1", partOfSpeech: "adjective", translation: "красный", word: "red" },
-    { category: "Animals", level: "A1", partOfSpeech: "noun", translation: "кошка", word: "cat" },
-    { category: "Food", level: "A1", partOfSpeech: "noun", translation: "яблоко", word: "apple" },
-    { category: "Time", level: "A1", partOfSpeech: "noun", translation: "сегодня", word: "today" },
-    { category: "Family", level: "A1", partOfSpeech: "noun", translation: "мать", word: "mother" },
-    { category: "Numbers", level: "A1", partOfSpeech: "number", translation: "один", word: "one" },
-    { category: "Common verbs", level: "A1", partOfSpeech: "verb", translation: "быть", word: "to be" }
-  ];
+  private firstCard: Card|null = null;
+  private secondCard: Card|null= null;
+  private isLocked = false;
+  private cdr = inject(ChangeDetectorRef);
+  private route = inject(ActivatedRoute);
+  private categoryService = inject(CategoryServiceMock);
 
-  public ngOnInit(): void {
-    this.createCards();
+  public ngOnInit():void {
+    const level = this.route.snapshot.paramMap.get('level');
+    const category = this.route.snapshot.paramMap.get('category');
+
+    if (level && category) {
+      this.words = this.categoryService.getWords(level, category);
+      this.createCards();
+    }
   }
 
   public createCards (): void {
-    this.WORDS.forEach((element, index) => {
+    this.words.forEach((element, index) => {
       this.cards.push({
         content: element.word,
         pairId: index,
@@ -43,7 +50,7 @@ export class MatchPairs implements OnInit {
       })
     });
 
-    this.WORDS.forEach((element, index) => {
+    this.words.forEach((element, index) => {
       this.cards.push({
         content: element.translation,
         pairId: index,
@@ -69,13 +76,58 @@ export class MatchPairs implements OnInit {
     this.easyMode = !this.easyMode
   }
 
-    public onCardClick(card: Card): void {
-    if (this.easyMode || card.opened || card.matched) {
+  public onCardClick(card: Card): void {
+    if (this.gameWon || this.isLocked || card.opened || card.matched) {
       return;
     }
 
     card.opened = true;
-    // здесь добавишь логику проверки пары (например, сравнивать с предыдущей выбранной картой)
+
+    if (this.firstCard === null) {
+      this.firstCard = card;
+    } else {
+      this.secondCard = card;
+      this.isLocked = true;
+
+      if (this.firstCard.pairId === this.secondCard.pairId) {
+        this.firstCard.matched = true;
+        this.secondCard.matched = true;
+        this.checkIfGameIsWon();
+        this.resetCards();
+      } else {
+        setTimeout(() => {
+          if(this.firstCard && this.secondCard){
+            this.firstCard.opened = false;
+            this.secondCard.opened = false;
+            this.resetCards();
+            this.cdr.markForCheck();
+          }}, 700);
+      }
+    }
+    this.cdr.markForCheck();
   }
 
+  public resetCards(): void {
+    this.firstCard = null;
+    this.secondCard = null;
+    this.isLocked = false;
+  }
+
+  public restartGame(): void {
+    this.gameWon = false;
+    this.isLocked = false;
+    this.firstCard = null;
+    this.secondCard = null;
+    this.easyMode = false;
+    this.cards = [];
+
+    this.createCards();
+    this.cdr.markForCheck();
+  }
+
+  private checkIfGameIsWon(): void {
+    if(this.cards.every(a => a.matched === true)){
+      this.gameWon = true;
+    }
+  }
 }
