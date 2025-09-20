c597b8cf22dc080b3ff0e8490f95023acac96103
TatsHats <6642017@gmail.com>
2025-09-20 10:53:03 +0300

    fix(test): fix test for user servis

--- src/app/core/services/user/user.spec.ts
+++ src/app/core/services/user/user.spec.ts
@@ -1,13 +1,21 @@
 import { TestBed } from '@angular/core/testing';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { UserService } from './user';
+import { firebaseConfig } from '../../firebase/firebase.config';
 
-import { User } from './user';
-
-describe('User', () => {
-  let service: User;
+describe('UserService', () => {
+  let service: UserService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(User);
+    TestBed.configureTestingModule({
+      providers: [
+        UserService,
+        provideFirebaseApp(() => initializeApp(firebaseConfig)),
+        provideFirestore(() => getFirestore()),
+      ],
+    });
+    service = TestBed.inject(UserService);
   });
 
   it('should be created', () => {
