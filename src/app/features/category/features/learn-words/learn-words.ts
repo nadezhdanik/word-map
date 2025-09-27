9c3b2942ef7e72f8f07447038f045f867a4fb9d9
nadezhdanik <nadezhdanik@gmail.com>
2025-09-27 13:33:09 +0300

    feat(learn-words): add basic layout

--- src/app/features/category/features/learn-words/learn-words.ts
+++ src/app/features/category/features/learn-words/learn-words.ts
@@ -1,10 +1,69 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CategoryServiceMock } from '../../../home/services/categories.service.mock';
+import { Card } from './models/card.model';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-learn-words',
-  imports: [],
+  imports: [MatIconModule, MatCardModule],
   templateUrl: './learn-words.html',
   styleUrl: './learn-words.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LearnWords {}
+export class LearnWords implements OnInit {
+  public level = '';
+  public category = '';
+  public currentCard = signal<Card>({ word: '', translation: '', showTranslation: false });
+
+  private cards: Card[] = [];
+  private currentIndex = signal(0);
+
+  private categoryService = inject(CategoryServiceMock);
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+
+  public ngOnInit(): void {
+    this.getCardData();
+    this.updateCurrentCard();
+  }
+
+  public goBack(): void {
+    this.router.navigate(['/category', this.level, this.category]);
+  }
+
+  public toggleTranslation(): void {
+    const card = this.currentCard();
+    this.currentCard.set({
+      ...card,
+      showTranslation: !card.showTranslation,
+    });
+  }
+
+  private getCardData(): void {
+    const level = this.route.snapshot.paramMap.get('level');
+    const category = this.route.snapshot.paramMap.get('category');
+
+    if (level && category) {
+      this.level = level;
+      this.category = category;
+
+      const words = this.categoryService.getWords(level, category);
+      this.cards = words.map((w) => ({
+        word: w.word,
+        translation: w.translation,
+        showTranslation: false,
+      }));
+    }
+  }
+
+  private updateCurrentCard(): void {
+    const index = this.currentIndex();
+    if (this.cards.length > 0 && index < this.cards.length) {
+      this.currentCard.set(this.cards[index]);
+    } else {
+      this.currentCard.set({ word: 'No more cards!', translation: '', showTranslation: false });
+    }
+  }
+}
