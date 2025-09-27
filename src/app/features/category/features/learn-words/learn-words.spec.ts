663af5330952ea8eab5fe78b46db90b8ebdb92d8
Nadezhda Memelova <nadezhdanik@gmail.com>
2025-09-27 17:18:57 +0300

    fix(learn-words): fix tests

--- src/app/features/category/features/learn-words/learn-words.spec.ts
+++ src/app/features/category/features/learn-words/learn-words.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LearnWords } from './learn-words';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('LearnWords', () => {
   let component: LearnWords;
@@ -9,6 +11,25 @@ describe('LearnWords', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [LearnWords],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            paramMap: of(
+              new Map([
+                ['level', 'A1'],
+                ['category', 'Food'],
+              ]),
+            ),
+            snapshot: {
+              paramMap: new Map([
+                ['level', 'A1'],
+                ['category', 'Food'],
+              ]),
+            },
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LearnWords);
